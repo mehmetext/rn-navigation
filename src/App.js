2f2b8f61c7ddf9b02fa901aa60e1ece6bb44b044
Mehmet Konukcu <mehmetext68@gmail.com>
2023-01-04 16:36:28 +0300

    react-navigation kuruldu

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {Text, SafeAreaView} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <SafeAreaView>
+        <Text>App</Text>
+      </SafeAreaView>
+    </NavigationContainer>
+  );
+}
